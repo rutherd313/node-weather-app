408f553980a90d1313e932cd2fb149e20e7425e9
John Dueno <djohnruther@yahoo.com>
2019-04-15 13:42:16 -0500

    add temp high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,12 +8,15 @@ const forecast = (latitude, longitude, callback) => {
         const temperature = body.currently.temperature;
         const precipProbability = body.currently.precipProbability;
         const bodyError = body.error;
+        const tempHigh = body.daily.data[0].temperatureHigh;
+        const tempLow = body.daily.data[0].temperatureLow;
         if (error) {
             callback("Unable to conncect to weather service", undefined)
         } else if (bodyError) {
             callback("Unable to find location", undefined);
         } else {
-            callback(undefined, summary + " It is currently " + temperature + " out. There is a " + precipProbability + "% chance of rain");
+            callback(undefined, summary + " It is currently " + temperature + " out. There is a " + precipProbability + "% chance of rain. " + 
+            "High for today is: " + tempHigh + "with a low of: " + tempLow);
         }
     });
 };
